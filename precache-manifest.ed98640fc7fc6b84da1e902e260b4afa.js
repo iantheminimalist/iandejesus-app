35f16e6b30625a105970e284ccd253aa5c27bf7a
Ian De Jesus <ian.dejesus.fontilea@gmail.com>
2020-10-15 00:16:14 -0700

    Updates

---
+++ precache-manifest.ed98640fc7fc6b84da1e902e260b4afa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "411b951768e2ab248ba6231a5c704ddc",
+    "url": "/iandejesus-app/index.html"
+  },
+  {
+    "revision": "66e6a4163e2f545144b2",
+    "url": "/iandejesus-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9a2fe0102663824a7479",
+    "url": "/iandejesus-app/static/js/2.c7f5502a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/iandejesus-app/static/js/2.c7f5502a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66e6a4163e2f545144b2",
+    "url": "/iandejesus-app/static/js/main.0ec96373.chunk.js"
+  },
+  {
+    "revision": "2c3657a603605a562df9",
+    "url": "/iandejesus-app/static/js/runtime-main.5c592b9c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/iandejesus-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
