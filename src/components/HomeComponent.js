cf1a93c12aee7b7925b4d7864bf2cc63b5aaead2
Ian De Jesus <ian.dejesus.fontilea@gmail.com>
2020-11-11 20:28:07 -0800

    changing image file

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -10,7 +10,7 @@ import Footer from './FooterComponent';
 
 
 function Home() {
-const profileImg = `iandejesus-app/assets/img/ian-profile.jpg`;
+const profileImg = `iandejesus-app/assets/img/ian-profile.JPG`;
 
     return (
     <div>
